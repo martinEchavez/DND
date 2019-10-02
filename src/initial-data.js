00148c284420b77628c58897564f945b98d87411
martinEchavez <martinechavez1994@gmail.com>
2019-10-01 23:46:50 -0500

    first commit

---
+++ src/initial-data.js
@@ -0,0 +1,17 @@
+const initialData = {
+    tasks: {
+        'task-1': {id: 'task-1', content: 'Example Task 1 '},
+        'task-2': {id: 'task-2', content: 'Example Task 2 '},
+        'task-3': {id: 'task-3', content: 'Example Task 3 '},
+        'task-4': {id: 'task-4', content: 'Example Task 4 '}
+    },
+    columns: {
+        'column-1': {
+            id: 'column-1',
+            title: 'To Do', 
+            taskIds: ['task-1', 'task-2', 'task-3', 'task-4']
+        },
+    },
+    columnOrder: ['column-1']
+};
+export default initialData;
